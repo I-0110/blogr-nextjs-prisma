b179d2a00ce11b0882664c87d96fef5bc759da1d
IvelisBecker <ivelisbecker@gmail.com>
2025-10-17 17:46:42 -0500

    Added a new post

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -8,11 +8,21 @@ export default async function handle(req, res) {
   const { title, content } = req.body;
 
   const session = await getSession({ req });
+
+  if (!session || !session.user?.email) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+
   const result = await prisma.post.create({
     data: {
       title: title,
       content: content,
-      author: { connect: { email: session?.user?.email } },
+      author: { 
+        connect: { 
+          email: session?.user?.email, 
+        },
+      },
     },
   });
   res.json(result);
